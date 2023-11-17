b2cc25036dd9d0e52c2414b1479bc599415b7ef2
Carmen <149541765+carmenvera03@users.noreply.github.com>
2023-11-17 15:32:35 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ let app = document.getElementById('typewriter');
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
- cursor: "<span style='color: #fb4993;'>|</span>",
+ cursor: "<span style='color: #E0AFA0;'>|</span>",
 });
  
 typewriter
   .pauseFor(2500)
-  .typeString('<span style="color: #fb4993;">Estudio programación web y estoy interesado en crear contenido relacionado con la tecnología.</span>')
+  .typeString('<span style="color: #E0AFA0;">Estudio programación web y estoy interesado en crear contenido relacionado con la tecnología.</span>')
   .pauseFor(200)
   .deleteChars(10)
   .start();
